ea14942700b5ed277abea02f63f5b2d747572348
satoshi <uglysatoshi@ya.ru>
2024-02-26 03:29:27 +0300

    complete

--- app/src/main/java/com/example/multy_lab_3/MainActivity.java
+++ app/src/main/java/com/example/multy_lab_3/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.multy_lab_3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static String activityText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final EditText editText = findViewById(R.id.editText);
+        Button button = findViewById(R.id.next);
+        button.setOnClickListener(view -> {
+            activityText = editText.getText().toString().isEmpty() ? "Вы не ввели данных :(" : String.format("Вы ввели:\n%s", editText.getText().toString());
+            Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+            startActivity(intent);
+        });
     }
 }
--- app/src/main/java/com/example/multy_lab_3/SecondActivity.java
+++ app/src/main/java/com/example/multy_lab_3/SecondActivity.java
@@ -2,7 +2,10 @@ package com.example.multy_lab_3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        TextView textView = findViewById(R.id.textView2);
+        textView.setText(MainActivity.activityText);
+        Button button = findViewById(R.id.back);
+        button.setOnClickListener(view -> {
+            Intent intent = new Intent(SecondActivity.this, MainActivity.class);
+            startActivity(intent);
+        });
     }
 }
